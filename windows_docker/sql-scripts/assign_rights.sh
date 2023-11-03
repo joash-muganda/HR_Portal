47e2f59325d890d54bf0464e146b9e61aab05430
alexazam <alexyszamo@gmail.com>
2023-11-02 19:40:59 -0700

    replaced line endings in sh scripts in windows docker with unix line endings

---
+++ windows_docker/sql-scripts/assign_rights.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# windows-specific code
+# Change ownership and permissions of /var/lib/mysql and /docker-entrypoint-initdb.d
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /docker-entrypoint-initdb.d
+# Start the original MySQL entrypoint
+exec /usr/local/bin/docker-entrypoint.sh "$@"
+
---
+++ windows_docker/sql-scripts/make_db_data.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# macOS-specific code
+# Load environment variables from .env file
+export $(grep -v '^#' env | xargs)
+
+# Set MySQL connection variables
+USER=$MYSQL_USER
+PASSWORD=$MYSQL_PASSWORD
+HOST=$MYSQL_HOST
+DATABASE=$MYSQL_DB
+# Directory where the dump files are located
+DUMP_DIR="./dump_files"
+
+# Change to the directory containing the dumps
+cd "$DUMP_DIR"
+
+# Loop through all .sql files in the specified directory
+for DUMP_FILE in *.dump
+do
+  echo "Importing $DUMP_FILE into $DATABASE"
+  mysql -u "$USER" -p"$PASSWORD" -h "$HOST" -P 3306 -D "$DATABASE" < "$DUMP_FILE"
+  if [ $? -eq 0 ]; then
+    echo "Imported $DUMP_FILE successfully."
+  else
+    echo "Error occurred during import of $DUMP_FILE"
+  fi
+done
