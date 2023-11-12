b8f70bd6e0f6bf34d287dc1a21788bb24d6a00c1
joash-muganda <113811567+joash-muganda@users.noreply.github.com>
2023-11-12 04:44:59 -0800

    Update base.py

--- models/base.py
+++ models/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = 'mysql+mysqlconnector://joash:Faryl%402023@localhost:3306/employees'
+DATABASE_URI = 'mysql+mysqlconnector://username:password@localhost:3306/employees'
 
 engine = create_engine(DATABASE_URI, echo=True)
 
